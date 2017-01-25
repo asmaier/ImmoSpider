989021c5b20645cf4597127280272f79329a3108
asmaier <asmaier@web.de>
2017-01-26 00:11:47 +0100

    Added parsing of javascript json result list.

--- totalimmo/items.py
+++ totalimmo/items.py
@@ -9,6 +9,22 @@ import scrapy
 
 
 class TotalimmoItem(scrapy.Item):
-    # define the fields for your item here like:
+	# define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    immo_id = scrapy.Field()
+    url = scrapy.Field()
+    title = scrapy.Field()
+    address = scrapy.Field()
+    city = scrapy.Field()
+    zip_code = scrapy.Field()
+    district = scrapy.Field()
+    contact_name = scrapy.Field()
+    media_count = scrapy.Field()
+    lat = scrapy.Field()
+    lng = scrapy.Field()
+    sqm  = scrapy.Field()
+    rent = scrapy.Field()
+    rooms = scrapy.Field()
+	# time_to = scrapy.Field()  # time to destination using transit or driving
+	
+
--- totalimmo/spiders/immoscout24.py
+++ totalimmo/spiders/immoscout24.py
@@ -1,24 +1,65 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import json
+from totalimmo.items import TotalimmoItem
 
 
 class Immoscout24Spider(scrapy.Spider):
     name = "immoscout24"
     allowed_domains = ["immobilienscout24.de"]
-    # start_urls = ['https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Berlin/Berlin']
-    start_urls = ['https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Berlin/Berlin/Lichterfelde-Steglitz_Nikolassee-Zehlendorf_Dahlem-Zehlendorf_Zehlendorf-Zehlendorf/2,50-/60,00-/EURO--800,00/-/-/']
+    start_urls = ['https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Berlin/Berlin']
+    # start_urls = ['https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Berlin/Berlin/Lichterfelde-Steglitz_Nikolassee-Zehlendorf_Dahlem-Zehlendorf_Zehlendorf-Zehlendorf/2,50-/60,00-/EURO--800,00/-/-/']
 
-    address_xpath = '//div[contains(@class, "result-list-entry__address")]/span/text()'
+    script_xpath = './/script[contains(., "IS24.resultList")]'
 
     def parse(self, response):
 
-    	for address in response.xpath(self.address_xpath).extract():
-    		print address
+        for line in response.xpath(self.script_xpath).extract_first().split('\n'):
+            if line.strip().startswith('model'):
+                immo_json = line.strip()
+                immo_json = json.loads(immo_json[7:-1])
+                
+                for result in immo_json["results"]:
+
+                    item = TotalimmoItem()
+
+                    item['immo_id'] = result['id']
+                    item['url'] = response.urljoin("/expose/" + str(result['id']))
+                    item['title'] = result['title']
+                    item['address'] = result['address']
+                    item['city'] = result['city']
+                    item['zip_code'] = result['zip']
+                    item['district'] = result['district']
+
+                    for attr in result['attributes']:
+                        if attr['title'] == "Kaltmiete":
+                            item['rent'] = attr['value']
+                        if attr['title'] == "Wohnfläche":
+                            item['sqm'] = attr['value']
+                        if attr['title'] == "Zimmer":
+                            item['rooms'] = attr['value']     
+
+                    try:
+                        item['contact_name'] = result['contactName']
+                    except:
+                        item['contact_name'] = None
+
+                    try:
+                        item['media_count'] = result['mediaCount']
+                    except:
+                        item['media_count'] = 0
+
+                    try:
+                        item['lat'] = result['latitude']
+                        item['lng'] = result['longitude']
+                    except:
+                        item['lat'] = None
+                        item['lng'] = None 
+                    # print item    
+                    yield item     
 
     	next_page = response.xpath('//div[@id = "pager"]/div/a/@href').extract()[-1]
     	print next_page
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)	
-
-    
