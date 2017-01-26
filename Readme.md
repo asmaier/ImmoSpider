b6329f5c278688726766d279c8e9959f6eb86f85
asmaier <asmaier@web.de>
2017-01-26 23:40:07 +0100

    Added search url as commandline argument. Cosmetic changes to parsing and logging. Added some comments.

---
+++ Readme.md
@@ -0,0 +1,3 @@
+# Usage
+
+   $ scrapy crawl immoscout24 -o appartments.csv -a url=https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Berlin/Berlin/Lichterfelde-Steglitz_Nikolassee-Zehlendorf_Dahlem-Zehlendorf_Zehlendorf-Zehlendorf/2,50-/60,00-/EURO--800,00/-/-/
--- totalimmo/spiders/immoscout24.py
+++ totalimmo/spiders/immoscout24.py
@@ -7,12 +7,20 @@ from totalimmo.items import TotalimmoItem
 class Immoscout24Spider(scrapy.Spider):
     name = "immoscout24"
     allowed_domains = ["immobilienscout24.de"]
-    start_urls = ['https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Berlin/Berlin']
+    # start_urls = ['https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Berlin/Berlin']
     # start_urls = ['https://www.immobilienscout24.de/Suche/S-2/Wohnung-Miete/Berlin/Berlin/Lichterfelde-Steglitz_Nikolassee-Zehlendorf_Dahlem-Zehlendorf_Zehlendorf-Zehlendorf/2,50-/60,00-/EURO--800,00/-/-/']
 
+    # The immoscout24 search results are stored as json inside their javascript. This makes the parsing very easy.
+    # I learned this trick from https://github.com/balzer82/immoscraper/blob/master/immoscraper.ipynb .
     script_xpath = './/script[contains(., "IS24.resultList")]'
+    next_xpath = '//div[@id = "pager"]/div/a/@href'
+
+    def start_requests(self):
+        yield scrapy.Request(self.url)
 
     def parse(self, response):
+
+        print response.url
 
         for line in response.xpath(self.script_xpath).extract_first().split('\n'):
             if line.strip().startswith('model'):
@@ -33,8 +41,8 @@ class Immoscout24Spider(scrapy.Spider):
 
                     for attr in result['attributes']:
                         if attr['title'] == "Kaltmiete":
-                            item['rent'] = attr['value']
-                        if attr['title'] == "Wohnfläche":
+                            item['rent'] = attr['value'][:-2]  # remove euro sign
+                        if attr['title'] == u"Wohnfläche":
                             item['sqm'] = attr['value']
                         if attr['title'] == "Zimmer":
                             item['rooms'] = attr['value']     
@@ -55,11 +63,10 @@ class Immoscout24Spider(scrapy.Spider):
                     except:
                         item['lat'] = None
                         item['lng'] = None 
-                    # print item    
+               
                     yield item     
 
-    	next_page = response.xpath('//div[@id = "pager"]/div/a/@href').extract()[-1]
-    	print next_page
+    	next_page = response.xpath(self.next_xpath).extract()[-1]
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)	
